73a0e1522bd85502a61918e2bc0d433d62d570e6
Katunin <Katunin_git@proton.me>
2025-03-27 09:10:19 +0200

    remove man section

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,9 +1,6 @@
 local g = vim.g
 -- telescope man pages sections
 local builtin = require('telescope.builtin')
-local function man_pages()
-  return builtin.man_pages({ sections = { "2", "3" } });
-end
 
 g.mapleader = " "
 vim.keymap.set('n', '<Leader>ve', '<Cmd>Ex<CR>', { silent = true })
@@ -14,7 +11,6 @@ vim.keymap.set('t', '<S-Tab>', '<C-\\><C-n>', { nowait = true })
 
 vim.keymap.set('n', '<leader>tgs', builtin.grep_string, { desc = 'Grep string under cursor' })
 vim.keymap.set('n', '<leader>tgl', builtin.live_grep, { desc = 'Grep' })
-vim.keymap.set('n', '<leader>tfm', man_pages, { desc = 'Find Man' })
 vim.keymap.set('n', '<leader>tff', builtin.find_files, { desc = 'Find file' })
 vim.keymap.set('n', '<leader>tfb', builtin.buffers, { desc = 'Find buffer' })
 vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle)
