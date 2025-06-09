3d3d0cab66792a0ab26216b37e3cb162ffd91284
Katunin <Katunin_git@proton.me>
2025-06-09 09:43:41 +0300

    change telescope colors

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,16 @@
 require('telescope').setup({
-defaults = {
-  border = false,
-}
+  defaults = {
+    border = false,
+  }
+})
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = function(args)
+    local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+    vim.api.nvim_set_hl(0, "TelescopeNormal", { fg = normal.fg, bg = normal.bg })
+    vim.api.nvim_set_hl(0, "TelescopePromptNormal", { fg = normal.fg, bg = normal.bg })
+
+    local prefix = vim.api.nvim_get_hl(0, { name = "TelescopePromptPrefix" })
+    vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { fg = prefix.fg, bg = normal.bg })
+  end
 })
